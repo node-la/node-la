e8f779e2f14ace4434f27546d555b4c351a34ef9
Joanna Tomassoni <jctomassoni@gmail.com>
2020-01-07 18:20:20 -0600

    get bio form on UserProfile component

--- client/src/components/Views/UserProfile.jsx
+++ client/src/components/Views/UserProfile.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
 import { makeStyles } from '@material-ui/core/styles';
-import { FormControl, FormLabel, Button, Paper, Input, InputLabel, FormHelperText, Container, TextField, Select } from '@material-ui/core';
+import { FormControl, FormLabel, Button, Paper, Input, InputLabel, Container, TextField } from '@material-ui/core';
 
 const useStyles = makeStyles(theme => ({
   root: {
@@ -9,22 +9,17 @@ const useStyles = makeStyles(theme => ({
   title: {
     flexGrow: 1,
   },
-  paper: {
+  button: {
+    background: 'linear-gradient(45deg, #00796b 30%, #43a047 90%)',
+    borderRadius: 4,
+    color: 'white',
     padding: theme.spacing(1),
-    margin: 'auto',
-    maxWidth: 700,
+    margin: '10px',
   },
-  // button: {
-  //   background: 'linear-gradient(45deg, #00796b 30%, #43a047 90%)',
-  //   borderRadius: 4,
-  //   color: 'white',
-  //   height: 48,
-  //   padding: '0 30px',
-  // },
   paper: {
     margin: theme.spacing(3),
     minWidth: 120,
-    padding: theme.spacing(3),
+    padding: theme.spacing(1),
     maxWidth: 700,
   }
 }));
@@ -35,12 +30,9 @@ const UserProfile = () => {
   return (
     <div>
       <Paper aria-labelledby="form-title" className={classes.paper}>
-        <FormLabel id="form-dialog-title"> Edit your bio </FormLabel>
+        <FormLabel className="formLabel" id="form-dialog-title"> Edit your bio </FormLabel>
           <TextField id="bio" label="Bio" type="bio" fullWidth />
-          {/* selection for neighborhoods */}
-          <TextField id="password" label="Password" type="password" fullWidth />
-        {/* buttons in dialog box */}
-          <Button color="primary">Save</Button>
+          <Button className={classes.button} color="primary">Save</Button>
       </Paper>
     </div>
   )
