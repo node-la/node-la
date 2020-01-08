5c45022c2f0a454a4499b84900d8d21593dee511
Joanna Tomassoni <jctomassoni@gmail.com>
2020-01-08 14:12:32 -0600

    fix calling updateUserBio when user clicks Save in profile

--- client/src/components/Views/UserProfile.jsx
+++ client/src/components/Views/UserProfile.jsx
@@ -37,7 +37,7 @@ const UserProfile = ({updateUserBio}) => {
 
   const handleClick = () => {
     setOpen(true);
-    () => updateUserBio(bio);
+    updateUserBio(bio);
   };
 
   const handleClose = (event, reason) => {
