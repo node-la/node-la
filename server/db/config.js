b072ef5c05946a9af825994d42c69398685f8ce9
Joanna Tomassoni <jctomassoni@gmail.com>
2020-01-10 18:03:20 -0600

    add cloud sql config

--- server/db/config.js
+++ server/db/config.js
@@ -1,4 +1,9 @@
-module.exports = {
+const dotenv = require('dotenv');
+dotenv.config();
+
+const { DB_HOST, DB_PASS } = process.env;
+
+const localMariaConfig = {
   user: 'root',
   host: 'localhost',
   password: '',
@@ -8,3 +13,16 @@ module.exports = {
     connectionTimeout: 1000
   }
 };
+
+const sqlConfig = {
+  user: 'root',
+  host: DB_HOST,
+  password: DB_PASS,
+  dialect: 'mariadb',
+  database: 'nodela'
+};
+
+module.exports = {
+  localMariaConfig: localMariaConfig,
+  sqlConfig: sqlConfig,
+}
